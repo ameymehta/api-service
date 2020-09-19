3c7a715ebe0815281f56b7358cc70e817986df4c
Mehta <amehta@Amey.fios-router.home>
2020-09-18 20:15:24 -0400

    Cleaned swagger page

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -31,43 +31,6 @@ const _listAllObjects = async (req: any, res: any) => {
 	}
 }
 
-/**
- * Handles an HTTP POST on /objects
- * 
- * @param {object} req 
- * @param {object} res 
- * 
- * @returns {Object} 
- */
-const _createObject = async (req: any, res: any) => {
-	try {
-		const response = responseUtil.handleResponse(constants.HTTP_STATUS_ACCEPTED, {});
-		res.status(response.status);
-		res.json(response.body);
-	} 
-	catch (err) {
-		return errorUtil.handleError(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, "Failed to create new object", res, req);
-	}
-}
-
-/**
- * Handles an HTTP DELETE on /objects/{objecttName}
- * 
- * @param {Object} req 
- * @param {Object} res
- */
-const _deleteObjectByName = async (req: any, res: any) => {
-	//Getting object name from path param
-	const { objectName } = req.params;
-	try {
-		const response = responseUtil.handleResponse(constants.HTTP_STATUS_ACCEPTED, {});
-		res.status(response.status);
-		res.json(response.body);
-	} catch (err) {
-		return errorUtil.handleError(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR, `Error terminating ${err}`, res, req);
-	}
-}
-
 // Public exports ------------------------------------------------------------->
 const router = express.Router({
 	caseSensitive: true,
@@ -76,9 +39,5 @@ const router = express.Router({
 
 //Get all objects
 router.get("/objects", auth, _listAllObjects);
-//Create new object
-router.post("/objects", auth, _createObject);
-//Delete an object
-router.delete("/objects/:objecttName", auth, _deleteObjectByName);
 
 module.exports = router;
